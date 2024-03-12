a39a61c6e30949d7ec5a733780040279f18a9297
Ly Nguyen <contact@minhlynguyenvo.com>
2024-03-12 14:01:38 +0100

    chore: remove superfluous query

--- src/main/kotlin/com/mlyngvo/JwtRefreshTokenRepository.kt
+++ src/main/kotlin/com/mlyngvo/JwtRefreshTokenRepository.kt
@@ -2,7 +2,6 @@ package com.mlyngvo
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 sealed interface JwtRefreshTokenRepository : JpaRepository<JwtRefreshTokenEntity, Long> {
@@ -12,6 +11,5 @@ sealed interface JwtRefreshTokenRepository : JpaRepository<JwtRefreshTokenEntity
     fun findByEmail(email: String): JwtRefreshTokenEntity?
 
     @Modifying(flushAutomatically = true)
-    @Query("delete from JwtRefreshTokenEntity where token = :token")
-    fun deleteByToken(@Param("token") token: String)
+    fun deleteByToken(token: String)
 }
