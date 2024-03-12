c48d01b878e3a67fc33b45315eec3c4789f7f29d
Ly Nguyen <contact@minhlynguyenvo.com>
2024-03-12 10:00:36 +0100

    chore: enable jpa

--- src/main/kotlin/com/mlyngvo/JwtAutoConfiguration.kt
+++ src/main/kotlin/com/mlyngvo/JwtAutoConfiguration.kt
@@ -3,12 +3,14 @@ package com.mlyngvo
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaRepositories
 @EnableConfigurationProperties(JwtProperties::class)
 class JwtAutoConfiguration {
 
