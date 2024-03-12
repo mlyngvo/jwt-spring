7195404b4f0c480d950c084dfb560733b9c687f7
Ly Nguyen <contact@minhlynguyenvo.com>
2024-03-12 10:53:58 +0100

    fix: migration query

--- src/main/kotlin/com/mlyngvo/JwtRefreshTokenMigration.kt
+++ src/main/kotlin/com/mlyngvo/JwtRefreshTokenMigration.kt
@@ -18,13 +18,14 @@ class JwtRefreshTokenMigration : JavaMigration {
         true
 
     override fun migrate(context: Context) {
-        val query = "create table if not exists jwt_refresh_token\n" +
+        val query = "drop table if exists jwt_refresh_token;\n" +
+                "create table jwt_refresh_token\n" +
                 "(\n" +
                 "    id bigint(9) unsigned not null primary key,\n" +
                 "    token text not null,\n" +
                 "    email varchar(255) not null\n" +
                 ");\n" +
-                "create unique index if not exists I_jrt_token on jwt_refresh_token (token);"
+                "create unique index I_jrt_token on jwt_refresh_token (token);"
         val statement = context.connection.prepareStatement(query)
         statement.execute()
     }
