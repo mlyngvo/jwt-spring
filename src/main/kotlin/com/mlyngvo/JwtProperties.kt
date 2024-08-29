ac730e84d06b2b45814d04a98b7136580dbc087d
Ly Nguyen <contact@minhlynguyenvo.com>
2024-08-29 14:33:32 +0200

    fix: properties

--- src/main/kotlin/com/mlyngvo/JwtProperties.kt
+++ src/main/kotlin/com/mlyngvo/JwtProperties.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("jwt")
 data class JwtProperties (
-    val accessTokenExp: Long,
-    val refreshTokenExp: Long,
     val publicKeyPath: String,
     val privateKeyPath: String,
+    val accessTokenExp: Long,
+    val refreshTokenExp: Long,
 )
