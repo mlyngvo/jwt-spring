6933aebcb82d7d3737b952915d2c5ad593830c8f
mlyngvo <contact@minhlynguyenvo.com>
2026-02-25 12:26:47 +0100

    fix: build issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,12 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "3.5.11"
     id("io.spring.dependency-management") version "1.1.7"
-    id("maven-publish")
 
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
+
+    `maven-publish`
 }
 
 publishing {
